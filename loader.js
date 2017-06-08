d077dc052b66c4dc5c46aa9a8c1eda903b3acc6a
DaveDx3 <david.dawson@dominionmarinemedia.com>
2017-06-07 21:32:13 -0700

    CDN

--- loader.js
+++ loader.js
@@ -3,7 +3,7 @@ var script = document.createElement('script');
 script.onload = function() {
   var link = document.createElement('link');
   link.rel="import";
-  link.href = "../banners/ipad-banner.html";
+  link.href = "https://simple-production-demo.github.io/banners/ipad-banner.html";
   document.getElementsByTagName('head')[0].appendChild(link);
   document.getElementsByClassName('story-body')[0].innerHTML="<ipad-banner></ipad-banner>";
 };
