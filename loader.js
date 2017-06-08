935cbfcbfde9a96c8f112b29ed64e3e40d94fa78
DaveDx3 <david.dawson@dominionmarinemedia.com>
2017-06-07 21:43:19 -0700

    CDN injector

--- loader.js
+++ loader.js
@@ -5,7 +5,7 @@ script.onload = function() {
   link.rel="import";
   link.href = "https://simple-production-demo.github.io/banners/ipad-banner.html";
   document.getElementsByTagName('head')[0].appendChild(link);
-  document.getElementsByClassName(bannerTgt)[0].parentNode.inser="<ipad-banner></ipad-banner>";
+  document.getElementsByClassName(window.bannerTgt)[0].parentNode.inser="<ipad-banner></ipad-banner>";
 };
 script.src = "https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.0.1/webcomponents-lite.js";
 document.getElementsByTagName('head')[0].appendChild(script);
