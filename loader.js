920c1aa1310f75271a98632dee287e21f47ae74f
DaveDx3 <davdawso@macc02sma7ag8wn.ca.bestbuy.com>
2017-06-09 14:38:13 -0700

    updates

--- loader.js
+++ loader.js
@@ -5,7 +5,7 @@ script.onload = function() {
   link.rel="import";
   link.href = "https://simple-production-demo.github.io/banners/ipad-banner.html";
   document.getElementsByTagName('head')[0].appendChild(link);
-  document.getElementsByClassName(bannerTgt)[0].innerHTML="<ipad-banner></ipad-banner>";
+  document.getElementsByClassName(bannerTgt)[0].innerHTML="<ipad-banner entry-id=134964></ipad-banner>";
 };
 script.src = "https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.0.1/webcomponents-lite.js";
 document.getElementsByTagName('head')[0].appendChild(script);
